401e9f38cb3380ab853e5a3bcb75de106f9b63fe
Milan Sony <milansonyofficial@gmail.com>
2024-02-16 21:33:58 +0530

    day4: use() middleware

--- day4/express_sample.js
+++ day4/express_sample.js
@@ -4,22 +4,22 @@ const path = require('path')
 const app = express();
 
 app.use(function(req, res, next){
-    console.log('Middleware')
-    next()
-})
+    console.log('Middleware');
+    next();
+});
 
 app.get('/', function(req, res){
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.get('/signup', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'signup.html'))
-})
+    res.sendFile(path.join(__dirname, 'signup.html'));
+});
 
 app.post('/signup', function(req, res){
-    res.send('Account Created Successfully')
-})
+    res.send('Account Created Successfully');
+});
 
 app.listen(7000, function(){
     console.log("Server Started");
-})
+});
