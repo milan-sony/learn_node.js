6680318df5568e849185d32d9610fa82d4413f6e
Milan Sony <milansonyofficial@gmail.com>
2024-02-19 20:06:38 +0530

    day7: promise.all()

---
+++ day7/promise_all.js
@@ -0,0 +1,21 @@
+const Promise = require('promise')
+
+function getName(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve('Milan')
+        }, 3000)
+    })
+}
+
+function getPhone(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve('001122334455')
+        }, 5000)
+    })
+}
+
+Promise.all([getName(), getPhone()]).then((result) =>{
+    console.log(result)
+})
