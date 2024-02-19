a48c587d2fe06088fac8bed52151e2d40a2e3730
Milan Sony <milansonyofficial@gmail.com>
2024-02-19 09:23:22 +0530

    change made

--- express_sample/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('about', { value: 'This is the about page'});
-});
-
-module.exports = router;
