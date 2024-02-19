c8c8d1088d4b3e2ff2b03d35584652709c6de5a4
Milan Sony <milansonyofficial@gmail.com>
2024-02-19 20:25:31 +0530

    day7: async-await

---
+++ day7/async-await.js
@@ -0,0 +1,28 @@
+const Promise = require('promise')
+
+function getName(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve('Milan')
+        }, 3000)
+    })
+}
+
+function getPhone(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve('001122334455')
+        }, 5000)
+    })
+}
+
+// Promise.all([getName(), getPhone()]).then((result) =>{
+//     console.log(result)
+// })
+
+async function getUser(){
+    let name = await getName()
+    console.log(name)
+}
+
+getUser()
